8f3fbcdd62562904bbd739559021c1c171533355
Charles Geng <gengqi.cn@gmail.com>
2021-02-02 10:52:40 +0900

    191 Optimized

--- 191.number-of-1-bits.py
+++ 191.number-of-1-bits.py
@@ -9,8 +9,8 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         result = 0
         while n != 0:
-            result += n & 1
-            n = n >> 1
+            n &= n - 1
+            result += 1
         return result
 
 
