bdd0065ce17ac317db5a73061400fae9292a57ec
Charles Geng <gengqi.cn@gmail.com>
2021-02-12 17:32:27 +0900

    Optimized Solution

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -7,22 +7,7 @@
 # @lc code=start
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        m = {}
-        for c in s:
-            if c not in m.keys():
-                m[c] = 1
-            else:
-                m[c] += 1
-
-        for i, c in enumerate(t):
-            if c not in m.keys() or m[c] < 1:
-                return False
-            else:
-                m[c] -= 1
-
-        return True
+        return sorted(s) == sorted(t)
 
 
 # @lc code=end
