db4b3a06128e3838cfb9bd90a0b49bb484cf8fe7
Charles Geng <gengqi.cn@gmail.com>
2021-02-15 14:36:33 +0900

    785 DFS solution

---
+++ 785.is-graph-bipartite.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=785 lang=python3
+#
+# [785] Is Graph Bipartite?
+#
+
+# @lc code=start
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        UNCOLORED, RED, GREEN = 0, 1, 2
+        color = [UNCOLORED] * n
+        valid = True
+
+        def dfs(node: int, c: int):
+            nonlocal valid
+            color[node] = c
+            cNei = GREEN if c == RED else RED
+            for neighbor in graph[node]:
+                if color[neighbor] == UNCOLORED:
+                    dfs(neighbor, cNei)
+                    if not valid:
+                        return
+                elif color[neighbor] != cNei:
+                    valid = False
+                    return
+
+        for i in range(n):
+            if color[i] == UNCOLORED:
+                dfs(i, RED)
+                if not valid:
+                    break
+
+        return valid
+
+
+# @lc code=end
