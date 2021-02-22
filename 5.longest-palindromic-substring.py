eb656c844245c879b815c50cc911fc42389bb356
Charles Geng <gengqi.cn@gmail.com>
2021-02-22 11:21:48 +0900

    Finish Q5

---
+++ 5.longest-palindromic-substring.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=5 lang=python3
+#
+# [5] Longest Palindromic Substring
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        sn = s[::-1]
+        l = len(sn)
+        maxSub = ""
+        for i in range(l):
+            if l - i < len(maxSub):
+                break
+            sub = sn[i]
+            for j in range(i + 1, l):
+                if sub + sn[j] in s:
+                    sub += sn[j]
+                else:
+                    break
+            if len(sub) > len(maxSub) and sub == sub[::-1]:
+                maxSub = sub
+            # print(sub)
+        return maxSub
+
+
+# @lc code=end
