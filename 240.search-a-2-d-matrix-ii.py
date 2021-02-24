7f4d35ba83d6fba38caf937aafa205510712ddc1
Charles Geng <gengqi.cn@gmail.com>
2021-02-24 12:08:13 +0900

    Search target from top-right to bottom-left

--- 240.search-a-2-d-matrix-ii.py
+++ 240.search-a-2-d-matrix-ii.py
@@ -7,34 +7,12 @@
 # @lc code=start
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # total rows
-        m = len(matrix)
-        # total columns
-        n = len(matrix[0])
-
-        i, j = 0, 0
-
-        while i < m and j < n:
-            if target > matrix[i][j]:
-                i += 1
-                j += 1
-            elif target == matrix[i][j]:
+        i = len(matrix[0]) - 1
+        for row in matrix:
+            while i and row[i] > target:
+                i -= 1
+            if row[i] == target:
                 return True
-            else:
-                break
-        l = j
-        while l < n:
-            for k in range(i):
-                if target == matrix[k][l]:
-                    return True
-            l += 1
-
-        l = i
-        while l < m:
-            for k in range(j):
-                if i < m and target == matrix[l][k]:
-                    return True
-            l += 1
         return False
 
 
