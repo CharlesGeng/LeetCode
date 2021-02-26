c2ff6841cff7d7e1494fdfe16db98187189cf9b3
Charles Geng <gengqi.cn@gmail.com>
2021-02-26 18:06:13 +0900

    Simplify the source code of Q946

--- 946.validate-stack-sequences.py
+++ 946.validate-stack-sequences.py
@@ -8,19 +8,12 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
         res = []
-        i = j = 0
-        while i < len(pushed):
-            if pushed[i] != popped[j]:
-                res.append(pushed[i])
-            else:
+        j = 0
+        for i in pushed:
+            res.append(i)
+            while res and res[-1] == popped[j]:
+                res.pop()
                 j += 1
-                while j < len(popped) and len(res) > 0:
-                    if popped[j] == res[len(res) - 1]:
-                        res.pop()
-                        j += 1
-                    else:
-                        break
-            i += 1
         return len(res) == 0
 
 
